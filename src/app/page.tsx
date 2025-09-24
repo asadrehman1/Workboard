318879c8164e68715408c29955c8b18773708f67
asadrehman1 <asadrehman7890661@gmail.com>
2025-09-24 09:39:33 -0700

    feat(auth): add current user, logout, and session middleware setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/features/auth/api/use-current-user";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrentUser();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data, isLoading, router]);
   return (
-    <div>
-      <Button variant="destructive">Click Me</Button>
+    <div className="text-center">
+      Only visible to authenticated users
+      <Button onClick={() => mutate()} variant="destructive">
+        Logout
+      </Button>
     </div>
   );
 }
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -5,8 +5,15 @@ import { zValidator } from "@hono/zod-validator";
 import { createAdminClient } from "@/lib/appwrite";
 import { loginSchema, registerSchema } from "../schemas";
 import { AUTH_COOKIE } from "../constants";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, async (c) => {
+    const user = c.get("user");
+    return c.json({
+      data: user,
+    });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
 
@@ -44,8 +51,12 @@ const app = new Hono()
       success: true,
     });
   })
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
+
     return c.json({
       success: true,
     });
