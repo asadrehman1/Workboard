3b1c4176560ae8a89525ca7d45acecad0ed4bd6b
asadrehman1 <asadrehman7890661@gmail.com>
2025-10-04 14:25:14 -0700

    feat(tasks): build API endpoints for fetching and creating tasks; add Zod schema for task form validation on frontend

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -4,6 +4,7 @@ import authRoute from "@/features/auth/server/route";
 import workspacesRoute from "@/features/workspaces/server/route";
 import membersRoute from "@/features/members/server/route";
 import projectsRoute from "@/features/projects/server/route";
+import tasksRoute from "@/features/tasks/server/route";
 
 const app = new Hono().basePath("/api");
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -12,6 +13,7 @@ const routes = app
   .route("/workspaces", workspacesRoute)
   .route("/members", membersRoute)
   .route("/projects", projectsRoute)
+  .route("/tasks", tasksRoute);
 
 export const GET = handle(app);
 export const POST = handle(app);
