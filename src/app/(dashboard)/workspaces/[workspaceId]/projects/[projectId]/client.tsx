6ffca78159709bb32552b562379166eb3cd40b99
asadrehman1 <asadrehman7890661@gmail.com>
2025-10-09 01:47:35 -0700

    feat(analytics): implemented project analytics page with API integration and visual insights

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { Analytics } from "@/components/analytics";
 import { PageError } from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { Button } from "@/components/ui/button";
 import { useGetProject } from "@/features/projects/api/use-get-project";
+import { useGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { useProjectId } from "@/features/projects/hooks/use-project-id";
 import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
@@ -12,7 +14,13 @@ import Link from "next/link";
 
 export const ProjectDetailClient = () => {
   const projectId = useProjectId();
-  const { data: project, isLoading } = useGetProject({ projectId });
+  const { data: project, isLoading: isLoadingProject } = useGetProject({
+    projectId,
+  });
+  const { data: projectAnalytics, isLoading: isLoadingAnalytics } =
+    useGetProjectAnalytics({ projectId });
+
+  const isLoading = isLoadingProject || isLoadingAnalytics;
 
   if (isLoading) {
     return <PageLoader />;
@@ -43,6 +51,7 @@ export const ProjectDetailClient = () => {
           </Button>
         </div>
       </div>
+      {projectAnalytics && <Analytics data={projectAnalytics} />}
       <TaskViewSwitcher hideProjectFilter />
     </div>
   );
