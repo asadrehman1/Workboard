6cd22aad8b23e6daea79c31979b001508576c3d5
asadrehman1 <asadrehman7890661@gmail.com>
2025-10-09 23:51:38 -0700

    fix: indent the code in the sign up from

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -44,9 +44,7 @@ export const SignUpCard = () => {
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
         <CardTitle className="text-2xl">Sign Up</CardTitle>
-        <CardDescription>
-          Welcome to Workboard!
-        </CardDescription>
+        <CardDescription>Welcome to Workboard!</CardDescription>
       </CardHeader>
       <div className="px-7">
         <DottedSeparator />
